528983f527551e174818f4a5087322347cfe9108
João Soares <jsoares.public@gmail.com>
2019-11-27 17:46:52 +0000

    Cool beans

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,9 @@
 <!DOCTYPE html>
 <html lang="pt">
     <head>
-        <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="<?= getAsset('css/bootstrap.min.css') ?>">
         <link rel="stylesheet" href="<?= getAsset('css/fontawesome.min.css') ?>">
 
