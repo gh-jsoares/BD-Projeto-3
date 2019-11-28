156cdab48c4b2bf643350254e8c287e10beea926
TiagoFonseca99 <47889514+TiagoFonseca99@users.noreply.github.com>
2019-11-28 16:30:55 +0000

    Update

---
+++ queries.sql
@@ -0,0 +1,44 @@
+-- Querie 1
+WITH TMP AS (
+	SELECT nome,
+	FROM
+	WHERE
+	UNION(
+		SELECT
+		FROM
+		NATURAL JOIN
+	)
+)
+SELECT nome
+FROM TEMP
+GROUP BY nome
+HAVING COUNT(*) >= ALL(
+	SELECT COUNT(*)
+	FROM TMP
+	GROUP BY nome)
+
+-- Querie 2
+WITH TMP AS (
+	SELECT i.email, i.anomalia_id, a.id, a.tem_anomalia_redacao, a.ts
+	FROM incidencia as i, anomalia as a
+	WHERE i.anomalia_id == a.id and a.tem_anomalia_redacao == 'false'
+	AND a.ts BETWEEN '2019-01-01 00:00:00' and '2019-06-30 23:59:59'
+)
+SELECT email, COUNT(*)
+FROM TMP
+GROUP BY email
+HAVING COUNT(*) >= ALL(
+	SELECT COUNT(*)
+	FROM TMP
+	GROUP BY email
+)
+
+-- Querie 3
+SELECT
+FROM
+WHERE
+
+-- Querie 4
+SELECT
+FROM
+WHERE
