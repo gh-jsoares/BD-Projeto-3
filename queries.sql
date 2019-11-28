9eafaaba65d79c90594d6db0bef38a5389af6cd0
TiagoFonseca99 <47889514+TiagoFonseca99@users.noreply.github.com>
2019-11-28 19:39:15 +0000

    Queries

--- queries.sql
+++ queries.sql
@@ -1,37 +1,34 @@
 -- Querie 1
 WITH TMP AS (
-	SELECT nome,
-	FROM
-	WHERE
-	UNION(
-		SELECT
-		FROM
-		NATURAL JOIN
-	)
+	SELECT *
+	FROM local_publico
+	NATURAL JOIN item
 )
 SELECT nome
-FROM TEMP
+FROM TMP
 GROUP BY nome
 HAVING COUNT(*) >= ALL(
 	SELECT COUNT(*)
 	FROM TMP
-	GROUP BY nome)
+	GROUP BY nome
+);
 
 -- Querie 2
 WITH TMP AS (
 	SELECT i.email, i.anomalia_id, a.id, a.tem_anomalia_redacao, a.ts
 	FROM incidencia as i, anomalia as a
-	WHERE i.anomalia_id == a.id and a.tem_anomalia_redacao == 'false'
+	WHERE i.anomalia_id = a.id and a.tem_anomalia_redacao = 'false'
 	AND a.ts BETWEEN '2019-01-01 00:00:00' and '2019-06-30 23:59:59'
 )
-SELECT email, COUNT(*)
-FROM TMP
-GROUP BY email
+SELECT TMP.email, COUNT(*)
+FROM TMP, utilizador_regular as ur
+WHERE TMP.email = ur.email
+GROUP BY TMP.email
 HAVING COUNT(*) >= ALL(
 	SELECT COUNT(*)
 	FROM TMP
 	GROUP BY email
-)
+); 
 
 -- Querie 3
 SELECT
